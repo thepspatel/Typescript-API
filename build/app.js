c2fa12758abcec73a823c9d9c1f06efbce6f6d16
Priyansh Patel <priyanshpatel0932@gmail.com>
2023-06-02 08:52:15 +0530

    commit

--- build/app.js
+++ build/app.js
@@ -4,11 +4,28 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const dbconnection_1 = __importDefault(require("./config/dbconnection"));
+const body_parser_1 = require("body-parser");
+const userRoutes = require('./routes/userRoute');
+const webRoutes = require('./routes/webRoute');
+const productRoutes = require('./routes/productRoute');
+const orderRoutes = require('./routes/orderRoute');
 const app = (0, express_1.default)();
 const port = 3000;
-app.get('/', (req, res) => {
-    res.send('Hello World');
+app.use((0, body_parser_1.json)());
+app.use((0, body_parser_1.urlencoded)({ extended: true }));
+dbconnection_1.default
+    .sync()
+    .then(() => {
+    console.log("Database connected Successfully");
+})
+    .catch((err) => {
+    console.log('Error', err);
 });
+app.use('/api', userRoutes);
+app.use('/', webRoutes);
+app.use('/api', productRoutes);
+app.use('/api', orderRoutes);
 app.listen(port, () => {
     console.log(`Connection established Successfully on port ${port}`);
 });
